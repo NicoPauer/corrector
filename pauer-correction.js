d20129ca6bb357e06aa7080ec271fb543080205c
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2024-05-17 15:20:42 -0300

    Update pauer-correction.js
    Init the corrector developing, test if corrector.fixing really add two values in the center of the list.

--- pauer-correction.js
+++ pauer-correction.js
@@ -1,1 +1,18 @@
-// Library for correction using mediation between extremes © 2024 NicoPauer
+/* Library for correction using mediation between extremes © 2024 NicoPauer
+ Object to encapsule the corrector logic and data for security reasons */
+
+var corrector = function(start, end)
+{
+    /* list that have opposite values between extremes 
+        e.g [1, -1] or [1, -3, 3, -1]
+        */
+      let values_interval = [start, end];
+      // Each call to the method fixing with the two new values improve precisition
+      this.fixing = function (value, different_value)
+      {
+        // Add values in the middle of the list
+        values_inertval[((values_interval.length - 1) / 2)] = value;
+        values_inertval[(((values_interval.length - 1) / 2) + 1)] = different_value; 
+      }
+      
+};
