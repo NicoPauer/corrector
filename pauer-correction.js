788a8e9687269950d193e9a9571f1b510ff8830e
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2024-05-17 17:20:16 -0300

    Update pauer-correction.js

--- pauer-correction.js
+++ pauer-correction.js
@@ -4,14 +4,17 @@
 var corrector = function(min_corrector, max_corrector)
 {
     /* Init JSON for be a good start point */
-      let copy = {min_corrector:1, max_corrector:10};
+      let copy = {
+                   min_corrector:1, 
+                   max_corrector:10
+                 };
     // Matize extremes values with intermediate values e.g: for 1 and -1 add zero.
       this.fixing = function (value, different_value)
       {
         // Add values to JSON with the init value 5 only for create this like JSON and be predictible and so long predictible as be as possible
         copy[value] = 5;
         copy[different_value] = 5;
-      }
+      };
 
      this.aprox = function(value)
      {
@@ -25,13 +28,13 @@ var corrector = function(min_corrector, max_corrector)
          {
            copy[value] = 3;
          }
-     }
+     };
  
      this.getJSON = function()
       {
        // Get JSON to modify externally if would be necessary copying result of it to an external variable
           return copy;
-      } 
+      }; 
      // Optimize with this
       delete copy, start, end;
 };
