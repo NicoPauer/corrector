e11a11c8109be0e9fc548847cd75079b31f20fda
Nicolas Pauer <89319674+NicoPauer@users.noreply.github.com>
2024-05-19 19:53:05 -0300

    Update form-controls.js
    Algorithm for get complementary color optimized, now works with color RGB codes list [R, G, B] format.

--- form-controls.js
+++ form-controls.js
@@ -2,99 +2,66 @@
 
 // Abstract logic
 
-function convertHex(integerNumber)
+function difRGB(first, second)
 {
-   // Give the hexadecimal convertion of the integer gived as parameter
-  /* 
+   // Make the difference between two 3-items arrays of integers with values of RGB colors channels
+   
+   let result = [];
 
-    Heleped by Google Gemini:
+   for (let index = 0; index < 3; index++)
+   {
+       // Add difference between each array component
+         result.push(first[index] - second[index]);
+   }
 
-        Prompt: "How convert a integer number in a hexadecimal number?"
-        
-      1. Manual Division Method:
-
-        This method involves repeated division by 16, the base of the hexadecimal system. Here's a step-by-step explanation:
-
-          Take the integer you want to convert.
-          
-          Divide the integer by 16.
-          
-          The remainder of this division is your rightmost hexadecimal digit. Convert this remainder to a hexadecimal digit (0-9 for decimal values, A-F for 10-15).
-          
-          The quotient becomes the new dividend for the next step.
-          
-          Repeat step 2 until the quotient becomes 0.
-        
-    The hexadecimal representation of your original number is the sequence of remainders you obtained, in reverse order
-    
-    */
-  // Hexadecimal digits
-  let digits = [(integerNumber % 16)];
- // Convertion result
-  let convertion = '';
- // This help to turn decimal results in hexadecimal digits
-  dict = {15 : 'f', 14 : 'e', 13 : 'd', 12 : 'c', 11 : 'b', 10 : 'a', 9:'9', 8:'8', 7:'7', 6:'6', 5:'5', 4:'4', 3:'3', 2:'2', 1:'1', 0:'0'};
- // Get the hexadecimal digits
-  let quotient = 16;
-  
-  while (quotient != 0)
-  {
-      digits.push(dict[(integerNumber % quotient)]);
-      quotient = (integerNumber / quotient);
-  }
- // Copy from digits list in revers order to convertion result
-  for (let index = (digits.length - 1); index >= 0; index--)
-  {
-      convertion += digits[index];
-  }
-// Give the resultant hexadecimal text value of the convertion  
-  return convertion;
-}
-
-function difHex(text_1, text_2)
-{
-  // With two hexadecimal strings calc the difference between them and give the hexadecimal string of the result
-  
-  text_1 = text_1.replace("#", "");
-
-  text_2 = text_2.replace("#", "");
-
-  result = convertHex(parseInt(text_1, 16) - parseInt(text_2, 16));
-  
-  return result;
+   return result;
 }
 
 function getColors(pickers)
 {
-    // Get color from color picker input list and return a haxadecimal string color codes list
+    // Get color from color picker input list and return a list with a sub-list with decimal values for red, green and blue
     let colors = [];
 
-    for (color in pickers)
+    for (let color in pickers)
     {
-      // The values by default is a string with the color in hexadecimal format
-        colors.push(color.value);
+       // Format color in [R, G, B] code style
+       
+        color = color.value.replace("#", "");
+
+        let colorArrayCode = []
+       // Each to characters of color turn into decimal value and add it to colorArrayCode
+        for (let index = 0; index < (color.length - 1); index++)
+        {
+           if ((index % 2) == 0)
+           {   
+              colorArrayCode.push(parseInt(color[index], 16));
+              colorArrayCode.push(parseInt(color[index + 1], 16));
+           }      
+        }
+       // Add color to the list
+        colors.push(colorArrayCode);
     }
 
     return colors;
 }
 
-function complementary(hexCode)
+function complementary(rgbCode)
 {
-    // Return hexadecimal color code of the complementary color passed as parameter
-      let code = "#ffffff";
+    // Return [R, G, B] color codes list of the complementary color passed as parameter in same formar
+      let code = [255, 255, 255];
     /* 
     
     By definition in the chromatic circle could look that the sum of two color give white 
 
         For example: 
         
-        The complementary color of yellow is blue, yellow has code #ffff00 and blue has code #0000ff sum that and 
-        get the code of the white #ffffff. for get the other only make the difference between white and the gived 
-        hexCode in this case #ffffff - #0000ff = #ffff00, #ffff00 is complementary to #0000ff.
+        The complementary color of yellow is blue, yellow has code [255, 255, 0] and blue has code [0, 0, 255] sum that and 
+        get the code of the white [255, 255, 255]. for get the other only make the difference between white and the gived 
+        hexCode in this case [255, 255, 255] - [0, 0, 255] = [255, 255, 0],  [255, 255, 0] is complementary to [0, 0, 255].
         
       */
   
-    code = "#" + difHex(code, hexCode);
+    code = difRGB(code, rgbCode);
   
     return code;
 }
